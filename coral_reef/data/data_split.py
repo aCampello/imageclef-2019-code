672ff2e210fd7479faab3f2192c2e6c3758968a7
Aljoscha Steffens <aljoscha.steffens@filament.ai>
2019-04-10 15:36:50 +0100

    changed the datasplit method a bit so that it does multiple runs and takes the best split. Updated the datafiles and the stats files according to the new split

--- coral_reef/data/data_split.py
+++ coral_reef/data/data_split.py
@@ -46,6 +46,14 @@ def _calc_mask_stats(mask_folder_path, colour_mapping):
 
 
 def calc_distance_metric(weights):
+    """
+    Creates a metric function that measures the distance of two vectors based on the given weights. The distance metric
+     is a weighted cosine distance.
+    :param weights: Vector that the other vectors will be weighted by. Needs to have the same dimension as the future
+    vectors
+    :return: distance metric
+    """
+
     def metric(v1, v2):
         return spatial.distance.cosine(v1 * weights, v2 * weights)
 
@@ -78,97 +86,90 @@ def calculate_split(mask_folder_path, colour_mapping, training_size=0.85):
     # index where to split the data into training and validation set
     split_index = int(len(names) * training_size)
 
-    # get initial split by doing some random splits and comparing them
-    print("Calculating initial split")
-    permutation_count = int(1e5)
+    # how many times should the procedure (random selection + swapping) be done
+    try_count = 10
 
-    min_dist = 1e5
-    min_distances = []
-    best_permutation = []
-    for i in range(permutation_count):
+    # variables for the outer loop
+    final_min_distance = 1e5
+    final_indices_train = []
+    final_indices_valid = []
 
-        permutation = np.random.permutation(len(names))
-        distrib_train = counts[permutation[:split_index]].sum(axis=0)
-        distrib_valid = counts[permutation[split_index:]].sum(axis=0)
+    for k in range(try_count):
+        print("Nr. {} of {} tries".format(k + 1, try_count))
+        # get initial split by doing some random splits and comparing them
+        print("Calculating initial split by random selection")
+        permutation_count = int(8e4)
 
-        distance = distance_metric(distrib_train, distrib_valid)
+        min_dist = 1e5
+        best_permutation = []
+        for i in range(permutation_count):
 
-        if distance < min_dist:
-            print("iteration {}, distance: {:.3e}".format(i + 1, distance))
-            min_dist = distance
-            best_permutation = permutation
-            min_distances.append(min_dist)
+            permutation = np.random.permutation(len(names))
+            distrib_train = counts[permutation[:split_index]].sum(axis=0)
+            distrib_valid = counts[permutation[split_index:]].sum(axis=0)
 
-    indices_train = best_permutation[:split_index].tolist()
-    indices_val = best_permutation[split_index:].tolist()
+            distance = distance_metric(distrib_train, distrib_valid)
 
-    print("Swapping")
-    # refine splits by swapping random entries if it decreases the distance
-    swap_count = int(1e4)
-    for i in range(swap_count):
+            if distance < min_dist:
+                print("iteration {}, distance: {:.3e}".format(i + 1, distance))
+                min_dist = distance
+                best_permutation = permutation
 
-        idx1 = indices_train[np.random.randint(len(indices_train))]
-        idx2 = indices_val[np.random.randint(len(indices_val))]
+        indices_train = best_permutation[:split_index].tolist()
+        indices_valid = best_permutation[split_index:].tolist()
 
-        temp_indices_train = [idx for idx in indices_train if idx != idx1] + [idx2]
-        temp_indices_val = [idx for idx in indices_val if idx != idx2] + [idx1]
+        print("Optimise split by swapping (optimal) indices")
+        swap_count = 100
+        for i in range(swap_count):
+            print("{} of {} swaps".format(i + 1, swap_count))
+            best_idx_train = -1
+            best_idx_valid = -1
+            temp_min_dist = min_dist
 
-        distrib_train = counts[temp_indices_train].sum(axis=0)
-        distrib_valid = counts[temp_indices_val].sum(axis=0)
+            for idx1 in indices_train:
+                for idx2 in indices_valid:
 
-        distance = distance_metric(distrib_train, distrib_valid)
+                    temp_indices_train = [idx for idx in indices_train if idx != idx1] + [idx2]
+                    temp_indices_val = [idx for idx in indices_valid if idx != idx2] + [idx1]
 
-        if distance < min_dist:
-            print("iteration {}, distance: {:.3e}".format(i + 1, distance))
+                    distrib_train = counts[temp_indices_train].sum(axis=0)
+                    distrib_valid = counts[temp_indices_val].sum(axis=0)
 
-            min_distances.append(distance)
-            min_dist = distance
-            indices_train = temp_indices_train
-            indices_val = temp_indices_val
+                    distance = distance_metric(distrib_train, distrib_valid)
 
-    swap_count = 100
-    for i in range(swap_count):
-        print("{} of {} opt swaps".format(i + 1, swap_count))
-        best_idx_train = -1
-        best_idx_val = -1
-        temp_min_dist = min_dist
+                    if distance < temp_min_dist:
+                        temp_min_dist = distance
+                        best_idx_train = idx1
+                        best_idx_valid = idx2
 
-        for idx1 in indices_train:
-            for idx2 in indices_val:
+            if best_idx_valid != -1:
+                # swap indices
+                indices_train = [idx for idx in indices_train if idx != best_idx_train] + [best_idx_valid]
+                indices_valid = [idx for idx in indices_valid if idx != best_idx_valid] + [best_idx_train]
 
-                temp_indices_train = [idx for idx in indices_train if idx != idx1] + [idx2]
-                temp_indices_val = [idx for idx in indices_val if idx != idx2] + [idx1]
+                min_dist = temp_min_dist
+                print("new min distance: {:.3e}".format(min_dist))
 
-                distrib_train = counts[temp_indices_train].sum(axis=0)
-                distrib_valid = counts[temp_indices_val].sum(axis=0)
+            else:
+                break
 
-                distance = distance_metric(distrib_train, distrib_valid)
-
-                if distance < temp_min_dist:
-                    temp_min_dist = distance
-                    best_idx_train = idx1
-                    best_idx_val = idx2
-
-        if best_idx_val != -1:
-            # swap indices
-            indices_train = [idx for idx in indices_train if idx != best_idx_train] + [best_idx_val]
-            indices_val = [idx for idx in indices_val if idx != best_idx_val] + [best_idx_train]
-
-            min_distances.append(temp_min_dist)
-            min_dist = temp_min_dist
+        # check if the current run of random picking + swapping resulted in a smaller distance
+        if min_dist < final_min_distance:
+            print("-"*50)
             print("new min distance: {:.3e}".format(min_dist))
+            print("-"*50)
+            final_min_distance = min_dist
+            final_indices_train = copy(indices_train)
+            final_indices_valid = copy(indices_valid)
 
-        else:
-            break
-
-    files_train = [names[idx] for idx in indices_train]
-    files_valid = [names[idx] for idx in indices_val]
+    files_train = [names[idx] for idx in final_indices_train]
+    files_valid = [names[idx] for idx in final_indices_valid]
 
     return files_train, files_valid
 
 
 if __name__ == "__main__":
-    mask_folder_path = "/home/aljo/filament/coral_reef/data/masks_super_small"
+    mask_folder_path = "/home/aljo/filament/coral_reef/data/masks"
 
     mappings_file_path = "/home/aljo/filament/coral_reef/data/colour_mapping.json"
     with open(mappings_file_path, "r") as fp:
