8a8496726597b951a442602cd4d34d5ac1fc1fb7
aCampello <a.campello@wellcome.ac.uk>
2020-11-26 09:42:33 +0000

    Update requirements and create a __main__

---
+++ coral_reef/__main__.py
@@ -0,0 +1,7 @@
+from coral_reef.ml.train import train
+
+if __name__ == "__main__":
+    # train(data_train, data_valid, image_base_dir, instructions)
+
+    # image_base_dir = presumabily a directory with all base images (data/images?)
+    print('heey')
